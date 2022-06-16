7bfc7e59e9b5dd8cf3799c5576ea2c8e0ad98b3f
Jack Moran <jvckmorvn@gmail.com>
2022-06-16 22:39:22 +0100

    Implemented and passed RSpec unit testing

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Forecast, type: :model do
     forecast = Forecast.new(postcode: 'l15fd', hot: 30, cold: 12)
     expect(forecast.format_postcode).to eq 'L1 5FD'
   end
+
+  it 'returns the correct temperature result for some given inputs' do
+    forecast = Forecast.new(postcode: 'l15fd', hot: 100, cold: -100).fetch_values
+    expect(forecast).to eq 'warm'
+  end
 end
