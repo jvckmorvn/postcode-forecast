ef82801a0c2a1b75b14dd853d07cedc0d24697c0
Jack Moran <jvckmorvn@gmail.com>
2022-06-16 22:30:05 +0100

    Ran linter

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Forecast, type: :model do
-  it "returns a correctly formatted postcode" do
-    forecast = Forecast.new(postcode: "l15fd", hot: 30, cold: 12)
-    expect(forecast.format_postcode).to eq "L1 5FD"
+  it 'returns a correctly formatted postcode' do
+    forecast = Forecast.new(postcode: 'l15fd', hot: 30, cold: 12)
+    expect(forecast.format_postcode).to eq 'L1 5FD'
   end
 end
